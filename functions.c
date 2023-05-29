b081ca493f547aa9ab34f8a22c3e77fab37f246b
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-05-29 08:42:57 -0700

    functions for printing integer value

--- functions.c
+++ functions.c
@@ -16,4 +16,31 @@
  */
 int number_printer(va_list args)
 {
-	int modulu_checker = 1
+	int modulu_checker = 1;
+	int count = 0;
+	int num_copy;
+
+	int num = va_arg(args, int);
+
+	if (num < 0)
+	{
+		count += _putchar('-');
+		num_copy = num * -1;
+	}
+	else
+	{
+		num_copy = num;
+	}
+
+	while ((num_copy / modulu_checker) > 9)
+	{
+		modulu_checker *= 10;
+	}
+
+	while (modulu_checker != 0)
+	{
+		count += _putchar('0' +( num_copy / modulu_checker));
+		modulu_checker /= 10;
+	}
+	 return (count);
+}
